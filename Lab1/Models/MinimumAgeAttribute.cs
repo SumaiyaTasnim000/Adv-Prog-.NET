be7820e5db292a6b0138f90cbe841977ae9183ef
X1 CARBON <X1 CARBON@DESKTOP-1DTH7E2>
2024-11-02 12:11:45 +0600

    Add project files.

---
+++ Lab1/Models/MinimumAgeAttribute.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class MinimumAgeAttribute : ValidationAttribute
+{
+    private readonly int _minAge;
+
+    public MinimumAgeAttribute(int minAge)
+    {
+        _minAge = minAge;
+        ErrorMessage = $"Age must be at least {_minAge} years.";
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value == null)
+        {
+            return new ValidationResult("Date of Birth is required.");
+        }
+
+        if (value is DateTime dob)
+        {
+            var today = DateTime.Today;
+            int age = today.Year - dob.Year;
+
+            // If the birthday hasn't occurred yet this year, subtract one from the age.
+            if (dob.Date > today.AddYears(-age)) age--;
+
+            if (age >= _minAge)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+
+        return new ValidationResult("Invalid date format.");
+    }
+}
---
+++ Lab1/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormIntro.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Please provide a name")]
+        [StringLength(10, ErrorMessage = "Name cannot exceed 10 characters")]
+        [RegularExpression(@"^[a-zA-Z.-]+$", ErrorMessage = "Name can only contain alphabets, '.' and '-'")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please provide a valid ID")]
+        [RegularExpression(@"^\d{2}-\d{5}-[1-3]$", ErrorMessage = "ID must be in the format 'xx-xxxxx-x', where xx is 00-99, xxxxx is 0-9, and the last digit is between 1-3.")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [MinimumAge(20, ErrorMessage = "Age must be at least 20 years.")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
